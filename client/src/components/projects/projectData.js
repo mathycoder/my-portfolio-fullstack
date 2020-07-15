49a1efe5e03dc621b608b3acba8ea80948267c16
Adam Sarli <adam.sarli@gmail.com>
2020-07-15 07:20:03 -0400

    adjusted demos to work with HTTPS

--- client/src/components/projects/projectData.js
+++ client/src/components/projects/projectData.js
@@ -9,7 +9,7 @@ export const myProjects = [
     appImage: "/images/student-agendas-pic.png",
     description: "Student Agendas was the tool I always wanted as an educator but could never quite find. It allows teachers to create individualized video agendas for students, as well as assess their understanding using reflection questions.  Every inch of the app is built with teachers and students in mind, such as a queue of 'To Be Graded' agenda items to ease grading for teachers.",
     githubUrl: "https://github.com/mathycoder/student-agendas-fullstack",
-    demoUrl: "http://www.youtube.com/embed/VXla-HpaPUI",
+    demoUrl: "https://www.youtube.com/embed/VXla-HpaPUI",
     bullets: [
       "Developed a Rails API backend with endpoints for classes, students, progressions, videos, and reflections.",
       "Implemented user interface employing React and normalized state-shaped Redux with CSS for styling.",
@@ -27,7 +27,7 @@ export const myProjects = [
     appImage: "/images/flexseats-pic.png",
     description: "As a teacher, I always struggled to create a seating chart that arranged students by both their ability and behavior.  To make life easy for teachers everywhere, FlexSeats does just that, using algorithms to sort students homogenously or heterogenously into pairs or groups.",
     githubUrl: "https://github.com/mathycoder/seating-chart",
-    demoUrl: null,
+    demoUrl: "https://www.youtube.com/embed/MsofdmXUr-g",
     animatedGif: "https://www.dropbox.com/s/m10qr7rp86mwm0g/flexseats-demo2-gif.gif?raw=1",
     bullets: [
       "Developed a Rails API backend with endpoints for teachers, klasses, and students.",
@@ -47,7 +47,7 @@ export const myProjects = [
     appImage: "/images/my-gradebook-pic.png",
     description: "I built MyGradebook to make standards-based grading easier and more intuitive for teachers.  The application makes it easy for teachers to see how their students are doing on the Common Core Standards.  Line and bar graphs give teachers immediate feedback on who knows what, and where to go next.  For any school hoping to make the jump to standards-based grading, this app would definitely help them on that journey!",
     githubUrl: "https://github.com/mathycoder/mygradebook-js",
-    demoUrl: "http://www.youtube.com/embed/6h50pqRM3aI",
+    demoUrl: "https://www.youtube.com/embed/6h50pqRM3aI",
     bullets: [
       "Developed a Rails API backend with endpoints for classes, students, learning targets, assignments, and grades.",
       "Integrated BCrypt and OmniAuth for server-side authentication.",
@@ -65,7 +65,7 @@ export const myProjects = [
     appImage: "/images/memory-lane-pic.png",
     description: "MemoryLane provides a more intimate way to share memories than Facebook or Instagram.  Users start by creating a lane with two or more people.  Then anyone in the lane can add a memory or contribute to other memories by adding their own recollections and photos.  The app is a great place for users to reminisce on old memories and recollections, or record brand new memories and share photos from new adventures!",
     githubUrl: "https://github.com/mathycoder/memory_lane",
-    demoUrl: "http://www.youtube.com/embed/d3hfnxCV2V4",
+    demoUrl: "https://www.youtube.com/embed/d3hfnxCV2V4",
     bullets: [
       "Developed a Sinatra backend with routes for users, lanes, memories, recollections, and images.",
       "Utilized Sqlite3 and ActiveRecord for database management.",
@@ -81,7 +81,7 @@ export const myProjects = [
     titleDescription: "Built with Python",
     appImage: "/images/pet-rescue-pic3.png",
     description: "Before attending Flatiron, I began my coding journey on Coursera learning Python.  After a course where we created Pong and a version of Asteroids, I decided to try and create a platformer from scratch.  The result is a fun side-scrolling video game, where my niece attempts to rescue all of her missing pets.  This 'pet' project allowed me to explore a visual GUI, something I hope to one day do within educational applications.",
-    demoUrl: "http://www.youtube.com/embed/ti942vuIiPI",
+    demoUrl: "https://www.youtube.com/embed/ti942vuIiPI",
     bullets: [
       "Explored object-oriented programming to organize behavior of main character, enemies, and game elements.",
       "Utilized SimpleGUI and recursion to animate sprites and other graphics.",
